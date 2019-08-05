2567adaae8982e19f11efa993fcd50517cf26c57
IMP1 <IMP1@mail.com>
2019-08-05 13:55:03 +0100

    Add increments for weekdays and month names

--- numerics.py
+++ numerics.py
@@ -1,17 +1,43 @@
+import re
 import sublime
 import sublime_plugin
 
 class IncrementCommand(sublime_plugin.TextCommand):
 
+    MONTHS = [
+        "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December"
+    ]
+
+    WEEKDAYS = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+
+    def wrap(self, table, value, delta):
+        current_index = table.index(value)
+        new_index = (current_index + delta) % len(table)
+        return table[new_index]
+
     def run(self, edit, delta):
         selection = self.view.sel()
         for region in selection:
             region_text = self.view.substr(region)
-            try:
-                result = str(int(region_text) + delta)
-            except Exception as e:
-                print(e)
-                result = region_text
+            result = region_text
+            if region_text in IncrementCommand.MONTHS:
+                result = self.wrap(IncrementCommand.MONTHS, region_text, delta)
+            elif region_text in IncrementCommand.WEEKDAYS:
+                result = self.wrap(IncrementCommand.WEEKDAYS, region_text, delta)
+            else:
+                try:
+                    result = str(int(region_text) + delta)
+                except Exception as e:
+                    print(e)
             self.view.replace(edit, region, result)
 
 
