66ff7f3db0c143c62c3f8169102bfecc51300899
IMP1 <huw_taylor@hotmail.co.uk>
2023-07-06 11:56:53 +0100

    Add InsertDate; Factor Formatting into constants

--- insert_datetime.py
+++ insert_datetime.py
@@ -4,10 +4,26 @@ import datetime
 
 class InsertTime(sublime_plugin.TextCommand):
 
+    TIME_FORMAT = "%H:%M"
+
     def run(self, edit):
-        current_time = datetime.datetime.now().strftime("%H%M")
+        current_time = datetime.datetime.now().strftime(TIME_FORMAT)
         for region in self.view.sel():
             if region.empty():
                 self.view.insert(edit, region.a, current_time)
             else:
                 self.view.replace(edit, region, current_time)
+
+
+class InsertDate(sublime_plugin.TextCommand):
+    
+    DATE_FORMAT = "%Y-%m-%d"
+
+    def run(self, edit):
+        current_time = datetime.datetime.now().strftime(DATE_FORMAT)
+        for region in self.view.sel():
+            if region.empty():
+                self.view.insert(edit, region.a, current_time)
+            else:
+                self.view.replace(edit, region, current_time)
+
